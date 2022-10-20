b6e3f43b2eb11a28f826adababecaa2c629fc494
S14mx <sergey.otryshko@gmail.com>
2022-10-19 21:21:28 -0700

    created frontend skeleton

---
+++ handydandy-frontend/pages/index.js
@@ -0,0 +1,25 @@
+import styles from '../styles/Home.module.css'
+import axios from 'axios'
+
+export default function Home() {
+  let new_res;
+  const fetcher = async (url) => {
+    try {
+      const res = await axios.get(url)
+      // console.log(res)
+      return res;
+    } catch (err) {
+      console.log("error")
+    }
+  };
+
+  const resp = fetcher('http://127.0.0.1:8000/api/v1/handydandy').then(resp => console.log(resp))
+
+
+  return (
+    <div className={styles.container}>
+      <p></p>
+
+    </div>
+  )
+}

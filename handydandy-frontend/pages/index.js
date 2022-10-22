ada49b2fa53d2423314a8d3ba217332d77067d9c
BrendonLH <Brendonhampton1988@gmail.com>
2022-10-22 11:40:46 -0700

    fixed all merge conflicts, merged brinas and sergii's branches

--- handydandy-frontend/pages/index.js
+++ handydandy-frontend/pages/index.js
@@ -1,37 +1,12 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { useUser, getSession } from '@auth0/nextjs-auth0';
+import { useUser } from '@auth0/nextjs-auth0';
 import styles from '../styles/Home.module.css'
 import NavBar from "../components/NavBar";
-
+import useApi from '../hooks/useApi';
 
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-  const [data, setData] = useState({})
-
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://handy-dandy.azurewebsites.net//api/v1/handydandy/', {
-          headers: {
-            Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkphM2I5cnBTOUxNVVpVU1AwczVHZiJ9.eyJpc3MiOiJodHRwczovL2Rldi00dmFtMDN3My51cy5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTYyNzczNjczNzYwMjgzMDA1NTkiLCJhdWQiOlsiaHR0cHM6Ly9kZXYtNHZhbTAzdzMudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2Rldi00dmFtMDN3My51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjY2MzkzODE1LCJleHAiOjE2NjY0ODAyMTUsImF6cCI6Ik1hWWdoSXNRa0t1WUNxb2pBU2tlaVhGbUdHMmw5WG16Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSJ9.ls4I1Fbphm7xLNduEr01fCoOwnWDQ_Kf8I4aKl5P_xzo4j2YjCaJMzHFrYpibyDurWwLMPob7NaqNmk6zf1GnfLX8C4ZclDToENS1A7YidNOBiqztAtfMU-AvIz4gQlTaYKWgCVp_h7rIqTBuYEU-h-cqQG229sxI-Eo-5Sr-X-48o3nj-ehIRVB3u41NEtIMHPO7ij04YoeMv-6ixThDKPByPt_jorkSVNqwAO1ThRSnrG7JGu-5BfcKnGyv1KN2kuYvfKhbs2esbbOlVwSdd7GDLPgRyaKbXKBD1RT-tWbTj7Q91L3GkSlfQKrTZIre3nu8KHU4JOxDQ8BpVje8g`
-          }
-        });
-        setData(response.data);
-      }
-      catch (error) {
-        if (error.response) {
-          console.log(error.response)
-        }
-      }
-    }
-    fetchData()
-  }, [])
-  console.log(user)
-
+  const { user } = useUser();
+  const { data, isLoading } = useApi('https://handy-dandy.azurewebsites.net//api/v1/handydandy/private')
 
   return (
     <>

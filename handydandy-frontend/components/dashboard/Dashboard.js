49d2be1a64e470558fada15fa5899984444881fa
BrendonLH <Brendonhampton1988@gmail.com>
2022-10-25 02:25:51 -0700

    got to figure out to make the form and create the user

--- handydandy-frontend/components/dashboard/Dashboard.js
+++ handydandy-frontend/components/dashboard/Dashboard.js
@@ -9,19 +9,15 @@ export default function Dashboard() {
     // console.log(isLoading);
     // console.log(data);
     // console.log(user);
-
-    function timer2(){
-        return <Register user={user}/>
-    }
-    
-    function timer(){
+  
+    function checkRegister(){
         if(isLoading === true){
             return <p>Loading</p>
         }
         else if(isLoading === false){
             if(data === undefined){
-                setTimeout(timer2, 5000);
-            }
+                return <Register user={user} />
+            }  
             else{
                 return <Registered user={data}/>
             }
@@ -32,7 +28,7 @@ export default function Dashboard() {
 
     
     return (
-            timer()
+            checkRegister()
     );
   }
 
--- handydandy-frontend/components/dashboard/Register.js
+++ handydandy-frontend/components/dashboard/Register.js
@@ -3,29 +3,51 @@ import { useState } from "react";
 
 export default function Register(props) {
 
-    const [userName, setUserName] = useState('')
-    console.log(props);
+    const [user, setUser] = useState({
+      // name
+    })
+    
+
     const updatedUser = {
-      // sid:props.user.sub.split("|")[1],
-      userName:userName,
+      sid:props.user.sub.split("|")[1],
+      name:name,
+      email:email,
+      address:address,
+      city:city,
+      zipcode:zipcode,
     }
 
     const registerUser = (event) => {
         event.preventDefault();
 
         // make post request to user route to register a new user
+        // after post request
         console.log(updatedUser);
       };
 
     const updateUser = (event) => {
-      setUserName(event.target.value);
-    }
+      setUser(event.target.value);
+      // spread here
+      
 
     return (
         <form onSubmit={registerUser}>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" value={userName} onChange={updateUser} />
-            <button type="submit">Submit</button>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="username" name="username" value={updatedUser.name} />
+
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" value={updatedUser.email} />
+
+            <label htmlFor="address">Address:</label>
+            <input type="text" id="address" name="address" value={updatedUser.address} />
+
+            <label htmlFor="city">City:</label>
+            <input type="text" id="city" name="city" value={updatedUser.city} />
+
+            <label htmlFor="zipcode">Zipcode:</label>
+            <input type="text" id="zipcode" name="zipcode" value={updatedUser.zipcode} />
+
+            <button onClick={updateUser} type="submit">Submit</button>
         </form>
     )
   }

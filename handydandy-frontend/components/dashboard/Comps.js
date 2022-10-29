81b3caddd21ae6a03ec75600a79b67d3cb40ad47
BrendonLH <Brendonhampton1988@gmail.com>
2022-10-28 21:05:27 -0700

    fixed register form to be more stylish and centerer again

--- handydandy-frontend/components/dashboard/Comps.js
+++ handydandy-frontend/components/dashboard/Comps.js
@@ -3,19 +3,19 @@ export default function Comps(props){
         <div className="flex flex-col w-full">
             <div className="">
                 <label className="p-2"> Hvac: </label>
-                <input className="" type="checkbox" name="hvac" value={false} defaultChecked={false} onChange={props.handleComps} />
+                <input className="lg rounded mb-2" type="checkbox" name="hvac" value={false} defaultChecked={false} onChange={props.handleComps} />
             </div>
             <div>
                 <label>Electrical: </label>
-                <input className="" type="checkbox" name="electrical" value={false} defaultChecked={false} onChange={props.handleComps} />
+                <input className="rounded" type="checkbox" name="electrical" value={false} defaultChecked={false} onChange={props.handleComps} />
             </div>
             <div>
                 <label>Carpentry: </label>
-                <input type="checkbox" name="carpentry" value={false} defaultChecked={false} onChange={props.handleComps} />
+                <input className="rounded" type="checkbox" name="carpentry" value={false} defaultChecked={false} onChange={props.handleComps} />
             </div>
             <div>
                 <label>Plumbing: </label>
-                <input type="checkbox" name="plumbing" value={false} defaultChecked={false} onChange={props.handleComps} />
+                <input className="rounded" type="checkbox" name="plumbing" value={false} defaultChecked={false} onChange={props.handleComps} />
             </div>
         </div>
     )
